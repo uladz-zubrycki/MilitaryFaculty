585bea8b151a59c56f33d7041f01f4461851927f
Anarjell <anarjell@gmail.com>
2013-10-26 13:02:01 +0300

    Attribute for DataProvider

---
+++ MilitaryFaculty.Common/MilitaryFaculty.Extensions/FormulaArgumentAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace MilitaryFaculty.Extensions
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class FormulaArgumentAttribute : Attribute
+    {
+        #region Class Properties
+
+        public string Name { get; private set; }
+
+        #endregion // Class Properties
+
+        #region Class Constructors
+
+        public FormulaArgumentAttribute(string name)
+        {
+            if (String.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException();
+            }
+
+            Name = name;
+        }
+
+        #endregion // Class Constructors
+    }
+}
