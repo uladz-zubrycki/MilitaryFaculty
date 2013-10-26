7074133225f58e4523e40de98ec6ec8a73812148
v-zubritsky <v.zubritsky@gmail.com>
2013-10-26 07:56:31 +0300

    entity context exceptions update

---
+++ MilitaryFaculty.Data/MilitaryFaculty.Data.Contract/Exceptions/DatabaseNotFoundException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace MilitaryFaculty.Data.Contract
+{
+    [Serializable]
+    public class DatabaseNotFoundException : Exception
+    {
+        public DatabaseNotFoundException()
+        {
+            // Empty
+        }
+
+        public DatabaseNotFoundException(string message) 
+            : base(message)
+        {
+            // Empty
+        }
+
+        public DatabaseNotFoundException(string message, Exception inner) 
+            : base(message, inner)
+        {
+            // Empty
+        }
+
+        protected DatabaseNotFoundException(SerializationInfo info, StreamingContext context) 
+            : base(info, context)
+        {
+            // Empty
+        }
+    }
+}
