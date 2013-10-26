894154ea03df4d18b101d072db538e09c089c5cb
v-zubritsky <v.zubritsky@gmail.com>
2013-10-26 07:57:55 +0300

    test project update

---
+++ _Tests/MilitaryFaculty.Data.Tests/EntityContext_Tests.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Configuration;
+using System.Data.Entity;
+using System.Linq;
+using MilitaryFaculty.Domain;
+using NUnit.Framework;
+
+// ReSharper disable InconsistentNaming
+namespace MilitaryFaculty.Data.Tests
+{
+    [TestFixture]
+    public class EntityContext_Tests
+    {
+        private EntityContext context;
+
+        [SetUp]
+        public void SetUp()
+        {
+            const string conName = "Current";
+
+            var connectionString = ConfigurationManager.ConnectionStrings[conName]
+                .ConnectionString;
+
+            Database.SetInitializer(new DropCreateDatabaseAlways<EntityContext>());
+            context = new EntityContext(connectionString);
+        }
+
+        [Test]
+        public void InitDatabase()
+        {
+            SeedCathedras(context);
+            SeedProfessors(context);
+            SeedConferences(context);
+            SeedBooks(context);
+        }
+
+        private void SeedCathedras(EntityContext context)
+        {
+            var cathedras = new[]
+                                {
+                                    new Cathedra("Кафедра тактики"),
+                                    new Cathedra("Кафедра связи"),
+                                    new Cathedra("Кафедра ПВО")
+                                };
+
+            foreach (var cathedra in cathedras)
+            {
+                context.Cathedras.Add(cathedra);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedProfessors(EntityContext context)
+        {
+            var cathedra = context.Cathedras
+                                  .AsQueryable()
+                                  .Single(c => c.Name == "Кафедра связи");
+
+            var professors = new[]
+                                 {
+                                     new Professor
+                                     {
+                                         FullName = new FullName("Сергей", "Николаевич", "Касанин"),
+                                         Cathedra = cathedra,
+                                         MilitaryRank = MilitaryRank.Colonel,
+                                         JobPosition = JobPosition.Professor,
+                                     },
+                                     new Professor
+                                     {
+                                         FullName = new FullName("Андрей", "Васильевич", "Кашкаров"),
+                                         Cathedra = cathedra,
+                                         MilitaryRank = MilitaryRank.Colonel,
+                                         JobPosition = JobPosition.Professor,
+                                     },
+                                     new Professor
+                                     {
+                                         FullName = new FullName("Роман", "Анатольевич", "Градусов"),
+                                         Cathedra = cathedra,
+                                         MilitaryRank = MilitaryRank.Colonel,
+                                         JobPosition = JobPosition.Professor,
+                                     },
+                                     new Professor
+                                     {
+                                         FullName = new FullName("Дюжов", "Юрьевич", "Геннадий"),
+                                         Cathedra = cathedra,
+                                         MilitaryRank = MilitaryRank.Colonel,
+                                         JobPosition = JobPosition.Professor,
+                                     },
+                                 };
+
+            foreach (var professor in professors)
+            {
+                context.Professors.Add(professor);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedConferences(EntityContext context)
+        {
+            var professor1 = context.Professors
+                                    .AsQueryable()
+                                    .Single(p => p.FullName.LastName == "Кашкаров");
+
+            var professor2 = context.Set<Professor>()
+                                    .AsQueryable()
+                                    .Single(p => p.FullName.LastName == "Касанин");
+
+            var conferences = new[]
+                                  {
+                                      new Conference
+                                      {
+                                          Name = "Конференция по вопросам морально-этического содержания " +
+                                                 "курса молодого бойца в частях специального назначения " +
+                                                 "военно-морского десанта Республики Беларусь",
+                                          Date = DateTime.Parse("01.07.1993"),
+                                          Curator = professor1,
+                                          ConferenceType = ConferenceType.University,
+                                          ConferenceReport = new ConferenceReport()
+                                                             {
+                                                                 OrganizationCorrectness = AccordanceLevel.Fully,
+                                                                 ReportMaterials = AccordanceLevel.None,
+                                                                 ThemeActuality = AccordanceLevel.Partly,
+                                                                 ResultsUsage = AccordanceLevel.Partly
+                                                             }
+                                      },
+                                      new Conference
+                                      {
+                                          Name = "Военно-научная конференция, посвященная 56-летию Великой Победы",
+                                          Date = DateTime.Parse("11.09.2001"),
+                                          Curator = professor1,
+                                          ConferenceType = ConferenceType.University,
+                                          ConferenceReport = new ConferenceReport()
+                                                             {
+                                                                 OrganizationCorrectness = AccordanceLevel.Fully,
+                                                                 ReportMaterials = AccordanceLevel.None,
+                                                                 ThemeActuality = AccordanceLevel.Partly,
+                                                                 ResultsUsage = AccordanceLevel.Partly
+                                                             }
+                                      },
+                                      new Conference
+                                      {
+                                          Name = "Вторая международная конференция",
+                                          Date = DateTime.Parse("13.08.2007"),
+                                          Curator = professor2,
+                                          ConferenceType = ConferenceType.University,
+                                          ConferenceReport = new ConferenceReport()
+                                                             {
+                                                                 OrganizationCorrectness = AccordanceLevel.Fully,
+                                                                 ReportMaterials = AccordanceLevel.None,
+                                                                 ThemeActuality = AccordanceLevel.Partly,
+                                                                 ResultsUsage = AccordanceLevel.Partly
+                                                             }
+                                      },
+                                  };
+
+            foreach (var conference in conferences)
+            {
+                context.Conferences.Add(conference);
+            }
+
+            context.SaveChanges();
+        }
+
+        private void SeedBooks(EntityContext context)
+        {
+            var professor1 = context.Professors
+                                    .AsQueryable()
+                                    .Single(p => p.FullName.LastName == "Кашкаров");
+
+            var professor2 = context.Professors
+                                    .AsQueryable()
+                                    .Single(p => p.FullName.LastName == "Касанин");
+
+            var books = new[]
+                            {
+                                new Book
+                                {
+                                    Name = "Учебник по специальной подготовке",
+                                    PagesCount = 228,
+                                    BookType = BookType.Schoolbook,
+                                    Author = professor1,
+                                },
+                                new Book
+                                {
+                                    Name = "Учебник по тактической подготовке",
+                                    PagesCount = 141,
+                                    BookType = BookType.Schoolbook,
+                                    Author = professor1,
+                                },
+                                new Book
+                                {
+                                    Name = "Как стать младшим офицером, " +
+                                           "начальником радиостанций коротковолновых " +
+                                           "малой мощности Р142-Н, Р145-БН за 21 день " +
+                                           "или курс молодого связиста для чайников",
+                                    PagesCount = 1309,
+                                    BookType = BookType.Tutorial,
+                                    Author = professor2,
+                                },
+                            };
+
+            foreach (var book in books)
+            {
+                context.Books.Add(book);
+            }
+
+            context.SaveChanges();
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
