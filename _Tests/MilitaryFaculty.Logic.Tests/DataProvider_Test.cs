e1ed435086d23d6c669f18b6e363e569e10c37e8
Anarjell <anarjell@gmail.com>
2013-11-02 13:17:36 +0300

    Logic tests

---
+++ _Tests/MilitaryFaculty.Logic.Tests/DataProvider_Test.cs
@@ -0,0 +1,24 @@
+﻿using NUnit.Framework;
+
+namespace MilitaryFaculty.Logic.Tests
+{
+    // ReSharper disable InconsistentNaming
+    [TestFixture]
+    class DataProvider_Test
+    {
+        private string atrName;
+
+        [SetUp]
+        public void SetUp()
+        {
+            atrName = "ProfsCount";
+        }
+
+        [Test]
+        public void TestXml()
+        {
+            var i = DataProvider.GetValue(atrName);
+        }
+    }
+    // ReSharper restore InconsistentNaming
+}
---
+++ _Tests/MilitaryFaculty.Logic.Tests/TableFormulas_Test.cs
@@ -0,0 +1,32 @@
+﻿// ReSharper disable InconsistentNaming
+
+using System;
+using MilitaryFaculty.Logic.XmlFormulasDomain;
+using MilitaryFaculty.Logic.XmlInfoDomain;
+using NUnit.Framework;
+
+namespace MilitaryFaculty.Logic.Tests
+{
+    [TestFixture]
+    class TableFormulas_Test
+    {
+        private TableFormulas tableFormulas;
+        private string path;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var basePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            path = "d:\\Other\\git_projects\\MilitaryFaculty\\MilitaryFaculty.Logic\\MilitaryFaculty.Logic.Services\\XmlTables\\";
+            path += "FirstTableFormulas.xml";
+        }
+
+        [Test]
+        public void TestXml()
+        {
+            tableFormulas = TableFormulas.Deserialize(path);
+        }
+    }
+}
+// ReSharper restore InconsistentNaming
